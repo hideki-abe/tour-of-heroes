d57b17ea34ac9c188a23ba0de85955abb40e3a86
hidekiabe <hideki-abe@hotmail.com>
2024-02-21 09:34:40 -0300

    Create a feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import {Hero} from "../hero";
 import {NgFor, UpperCasePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {HEROES} from "../mock-heroes";
+import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,8 @@ import {HEROES} from "../mock-heroes";
   imports: [
     UpperCasePipe,
     FormsModule,
-    NgFor
+    NgFor,
+    HeroDetailComponent
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'

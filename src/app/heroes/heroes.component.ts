03f2aba289de2909b9d931560b40b40832ef3447
hidekiabe <hideki-abe@hotmail.com>
2024-02-21 09:12:37 -0300

    display a selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import {Hero} from "../hero";
-import {UpperCasePipe} from "@angular/common";
+import {NgFor, UpperCasePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
     UpperCasePipe,
-    FormsModule
+    FormsModule,
+    NgFor
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelected(hero: Hero){
+    this.selectedHero = hero;
+    console.log(hero.name);
   }
 }
